f8c729e48e98ca7bcd8d80f83277034f6c9ea75c
Michael Calasanz <darthmdc@gmail.com>
2019-05-11 17:18:23 -0700

    Add project files.

---
+++ MVVM/UWP/TheHumbleDialogBox.Core/ViewModel/MainPageViewModel.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TheHumbleDialogBox.Core.ViewModel
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+        private const string ERROR_MESSAGE_BAD_SSN = "Bad SSN.";
+        private const string ERROR_MESSAGE_BAD_NAME = "Bad name.";
+
+        private Person _person;
+
+        public MainPageViewModel()
+        {
+            _person = new Person();
+        }
+
+        public string Name
+        {
+            get { return _person.Name; }
+            set
+            {
+                _person.Name = value;
+            }
+        }
+
+        public string Ssn
+        {
+            get { return _person.Ssn; }
+            set
+            {
+                _person.Ssn = value;                
+            }
+        }
+
+        private string _errorMessage;
+        public string ErrorMessage
+        {
+            get { return _errorMessage; }
+            set
+            {
+                _errorMessage = value;
+                OnPropertyChanged("ErrorMessage");
+            }
+        }
+
+        public bool OnOk()
+        {
+            if (IsValidName() == false)
+            {
+                ErrorMessage = ERROR_MESSAGE_BAD_NAME;
+                return false;
+            }
+
+            if (IsValidSSN() == false)
+            {
+                ErrorMessage = ERROR_MESSAGE_BAD_SSN;
+                return false;
+            }
+
+            ErrorMessage = string.Empty;
+            return true;
+        }
+
+        private bool IsValidName()
+        {
+            return Name.Length > 0;
+        }
+
+        private bool IsValidSSN()
+        {
+            string pattern = @"^\d{3}-\d{2}-\d{4}$";
+            return Regex.IsMatch(Ssn, pattern);
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged(string property)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Ssn { get; set; }
+
+    }
+}
---
+++ MVVM/UWP/TheHumbleDialogBox.UnitTests/UnitTest.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TheHumbleDialogBox.Core.ViewModel;
+
+namespace TheHumbleDialogBox.UnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+
+        [TestMethod]
+        public void NameOk()
+        {
+            var mainPage = new MainPageViewModel();
+            mainPage.Name = "John Smith";
+            mainPage.Ssn = "123-45-6782";
+
+            var result = mainPage.OnOk();
+
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void BlankOk()
+        {
+            var mainPage = new MainPageViewModel();
+            mainPage.Name = "John Smith";
+            mainPage.Ssn = "123-45-678";
+
+            var result = mainPage.OnOk();
+
+            mainPage.Name = "John Smith";
+            mainPage.Ssn = "123-45-6782";
+
+            result = mainPage.OnOk();
+
+            Assert.AreEqual(string.Empty, mainPage.ErrorMessage);
+        }
+
+        [TestMethod]
+        public void BadName()
+        {
+            var mainPage = new MainPageViewModel();
+            mainPage.Name = "";
+            mainPage.Ssn = "123-45-6782";
+
+            var result = mainPage.OnOk();
+
+            Assert.AreEqual(mainPage.ErrorMessage, "Bad name.");
+        }
+
+        [TestMethod]
+        public void BadSsn()
+        {
+            var mainPage = new MainPageViewModel();
+            mainPage.Name = "John Smith";
+            mainPage.Ssn = "";
+
+            var result = mainPage.OnOk();
+
+            Assert.AreEqual(mainPage.ErrorMessage, "Bad SSN.");
+        }
+    }
+}
