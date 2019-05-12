c67a62ec4131591ce540dd19049cd4fd0ea54bd8
Michael Calasanz <darthmdc@gmail.com>
2019-05-11 18:17:32 -0700

    Added mvc, winform projects

---
+++ MVC/TheHumbleDialogBox.MVC.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TheHumbleDialogBox.MVC.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
